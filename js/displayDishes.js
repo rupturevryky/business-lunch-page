50fa0eed4ea3fa0f3e68cde898487fe747a2143f
Rupturevryky <vladislav.m.vlad@gmail.com>
2024-11-08 19:03:48 +0300

    lab5

--- js/displayDishes.js
+++ js/displayDishes.js
@@ -7,7 +7,77 @@ document.addEventListener('DOMContentLoaded', () => {
     // Контейнеры для секций с блюдами
     const soupContainer = document.querySelector('section:nth-of-type(1) .dish_block');
     const mainContainer = document.querySelector('section:nth-of-type(2) .dish_block');
-    const drinkContainer = document.querySelector('section:nth-of-type(3) .dish_block');
+    const saladsContainer = document.querySelector('section:nth-of-type(3) .dish_block');
+    const drinkContainer = document.querySelector('section:nth-of-type(4) .dish_block');
+    const dessertsContainer = document.querySelector('section:nth-of-type(5) .dish_block');
+
+    const soupHeader = document.querySelector('section:nth-of-type(1) .dish_header');
+    const mainHeader = document.querySelector('section:nth-of-type(2) .dish_header');
+    const saladsHeader = document.querySelector('section:nth-of-type(3) .dish_header');
+    const drinkHeader = document.querySelector('section:nth-of-type(4) .dish_header');
+    const dessertsHeader = document.querySelector('section:nth-of-type(5) .dish_header');
+
+    let menu = {
+        soup: {
+            State: '',
+            Btns: ["мясной", "рыбный", "вегетарианский"]
+        },
+        main: {
+            State: '',
+            Btns: ["мясное", "рыбное", "вегетарианское"]
+        },
+        salads: {
+            State: '',
+            Btns: ["мясной", "рыбный", "вегетарианский"]
+        },
+        drink: {
+            State: '',
+            Btns: ["холодный", "горячий"],
+        },
+        desserts: {
+            State: '',
+            Btns: ["маленькая порция", "средняя порция", "большая порция"]
+        }
+    }
+
+    // Функция для создания кнопок типов блюд
+    const createDishKind = (kinds, category) => {
+        const nav = document.createElement('nav');
+        nav.className = 'dish_btns';
+
+        const cheange_kind = (event) => {
+
+            const buttons = nav.querySelectorAll(`.${category}`);
+            buttons.forEach(button => {
+                button.style.backgroundColor = ''; // Сбрасываем цвет
+            });
+
+            kinds.State = event.target.name;
+            event.target.style.backgroundColor = "lightgreen"
+
+            soupContainer.innerHTML = ""
+            mainContainer.innerHTML = ""
+            saladsContainer.innerHTML = ""
+            drinkContainer.innerHTML = ""
+            dessertsContainer.innerHTML = ""
+            insert_cards();
+            btn_order_update();
+        }
+
+        kinds.Btns.forEach(word => {
+            const btn = document.createElement('button'); // Создаем новую кнопку для каждого слова
+            btn.style.padding = "5px"
+            btn.style.borderRadius = "15px"
+            btn.classList.add('dish_nav_button'); // Добавляем первый класс к кнопке
+            btn.classList.add(category); // Добавляем второй класс к кнопке
+            btn.addEventListener('click', cheange_kind); // Добавляем обработчик события клика
+            btn.name = word; // Устанавливаем имя кнопки
+            btn.innerHTML = word; // Устанавливаем текст кнопки
+            nav.appendChild(btn); // Добавляем кнопку в навигацию
+        });
+
+        return nav; // Возвращаем навигацию с кнопками
+    };
 
     // Функция для создания карточки блюда
     const createDishCard = (dish) => {
@@ -25,14 +95,31 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     // Разделяем блюда по категориям и добавляем в соответствующие контейнеры
-    sortedDishes.forEach(dish => {
-        const card = createDishCard(dish);
-        if (dish.category === 'soup') {
-            soupContainer.appendChild(card);
-        } else if (dish.category === 'main') {
-            mainContainer.appendChild(card);
-        } else if (dish.category === 'drink') {
-            drinkContainer.appendChild(card);
-        }
-    });
+    const insert_cards = () => {
+        sortedDishes.forEach(dish => {
+            const card = createDishCard(dish);
+            if (dish.category === 'soup') {
+                if ((menu.soup.State && dish.kind == menu.soup.State) || !menu.soup.State)
+                    soupContainer.appendChild(card);
+            } else if (dish.category === 'main_course') {
+                if ((menu.main.State && dish.kind == menu.main.State) || !menu.main.State)
+                    mainContainer.appendChild(card);
+            } else if (dish.category === 'salads_starters') {
+                if ((menu.salads.State && dish.kind == menu.salads.State) || !menu.salads.State)
+                    saladsContainer.appendChild(card);
+            } else if (dish.category === 'beverages') {
+                if ((menu.drink.State && dish.kind == menu.drink.State) || !menu.drink.State)
+                    drinkContainer.appendChild(card);
+            } else if (dish.category === 'desserts') {
+                if ((menu.desserts.State && dish.kind == menu.desserts.State) || !menu.desserts.State)
+                    dessertsContainer.appendChild(card);
+            }
+        });
+    }
+    insert_cards();
+    soupHeader.appendChild(createDishKind(menu.soup));
+    mainHeader.appendChild(createDishKind(menu.main));
+    saladsHeader.appendChild(createDishKind(menu.salads));
+    drinkHeader.appendChild(createDishKind(menu.drink));
+    dessertsHeader.appendChild(createDishKind(menu.desserts));
 });
--- js/orderForm.js
+++ js/orderForm.js
@@ -1,8 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const selectedDishes = { soup: null, main: null, drink: null };
-    const orderCostDisplay = document.querySelector('#order-cost');
+
     const orderSection = document.querySelector('.customer-order');
-    const customerComment = document.querySelector('.customer-comment');
     const textarea = document.getElementById('customer-comment');
 
     textarea.addEventListener('input', function () {
@@ -14,63 +12,80 @@ document.addEventListener('DOMContentLoaded', () => {
         else
             this.style.height = 400 + 'px';
     });
-
-    // Функция для обновления блока с выбранными блюдами и стоимостью
-    const updateOrderDisplay = () => {
-        let totalCost = 0;
-        let somethingSelected = false;
-
-        // Убираем сообщение "Ничего не выбрано", если оно есть
-        const nothingSelectedElement = document.querySelector('.nothing-selected');
-        if (nothingSelectedElement) {
-            nothingSelectedElement.remove();
-        }
-
-        // Проходим по категориям: суп, главное блюдо, напиток
-        ['soup', 'main', 'drink'].forEach(category => {
-            const selectedDish = selectedDishes[category];
-            const displayElement = document.querySelector(`#${category}-order`);
-
-            // Если блюдо выбрано, показываем его, иначе показываем текст "Блюдо не выбрано"
-            if (selectedDish) {
-                displayElement.innerHTML = `<strong>${category === 'soup' ? 'Суп' : category === 'main' ? 'Главное блюдо' : 'Напиток'}:<br></strong> ${selectedDish.name} ${selectedDish.price}₽`;
-                displayElement.style.display = 'block'; // Отображаем категорию
-                totalCost += selectedDish.price;
-                somethingSelected = true; // Помечаем, что хотя бы одно блюдо выбрано
-            } else {
-                // if (somethingSelected) {
-                displayElement.style.display = 'block'; // Отображаем пустые категории только если что-то выбрано
-                // } else {
-                //     displayElement.style.display = 'none'; // Скрываем, если ничего не выбрано
-                // }
-            }
-        });
-
-        // Если ни одно блюдо не выбрано, отображаем сообщение "Ничего не выбрано"
-        if (!somethingSelected) {
-            document.querySelectorAll('.order-category').forEach(category => {
-                category.style.display = 'none'; // Скрываем все категории
-            });
-            if (!document.querySelector('.nothing-selected')) {
-                customerComment.insertAdjacentHTML('beforebegin', `<p class="nothing-selected"><br>Ничего не выбрано</p>`);
-            }
-        }
-
-        // Обновляем итоговую стоимость заказа
-        if (totalCost > 0) {
-            orderCostDisplay.textContent = `Стоимость заказа: ${totalCost}₽`;
-            orderCostDisplay.style.display = 'block'; // Показать стоимость, если есть сумма
-        } else {
-            orderCostDisplay.style.display = 'none'; // Скрыть стоимость, если сумма 0
-        }
-    };
-
     // Обработка выбора блюда
+    btn_order_update()
+    // Инициализация при загрузке страницы
+    updateOrderDisplay();
+});
+
+const selectedDishes = { soup: null, main_course: null, beverages: null, salads_starters: null, desserts: null };
+
+// Функция для обновления блока с выбранными блюдами и стоимостью
+const updateOrderDisplay = () => {
+    const orderCostDisplay = document.querySelector('#order-cost');
+    const customerComment = document.querySelector('.customer-comment');
+    let totalCost = 0;
+    let somethingSelected = false;
+
+    // Убираем сообщение "Ничего не выбрано", если оно есть
+    const nothingSelectedElement = document.querySelector('.nothing-selected');
+    if (nothingSelectedElement) {
+        nothingSelectedElement.remove();
+    }
+
+    // Проходим по категориям: суп, главное блюдо, напиток
+    ['soup', 'main_course', 'beverages', 'salads_starters', 'desserts'].forEach(category => {
+        const selectedDish = selectedDishes[category];
+        const displayElement = document.querySelector(`#${category}-order`);
+
+        // Если блюдо выбрано, показываем его, иначе показываем текст "Блюдо не выбрано"
+        if (selectedDish) {
+            let dish;
+            if (category == "soup") dish = "Суп"
+            if (category == "main_course") dish = "Главное блюдо"
+            if (category == "beverages") dish = "Напиток"
+            if (category == "salads_starters") dish = "Салат или стартер"
+            if (category == "desserts") dish = "Десерт"
+            displayElement.innerHTML = `<strong>${dish}:<br></strong> ${selectedDish.name} ${selectedDish.price}₽`;
+            displayElement.style.display = 'block'; // Отображаем категорию
+            totalCost += selectedDish.price;
+            somethingSelected = true; // Помечаем, что хотя бы одно блюдо выбрано
+        } else {
+            // if (somethingSelected) {
+            displayElement.style.display = 'block'; // Отображаем пустые категории только если что-то выбрано
+            // } else {
+            //     displayElement.style.display = 'none'; // Скрываем, если ничего не выбрано
+            // }
+        }
+    });
+
+    // Если ни одно блюдо не выбрано, отображаем сообщение "Ничего не выбрано"
+    if (!somethingSelected) {
+        document.querySelectorAll('.order-category').forEach(category => {
+            category.style.display = 'none'; // Скрываем все категории
+        });
+        if (!document.querySelector('.nothing-selected')) {
+            customerComment.insertAdjacentHTML('beforebegin', `<p class="nothing-selected"><br>Ничего не выбрано</p>`);
+        }
+    }
+
+    // Обновляем итоговую стоимость заказа
+    if (totalCost > 0) {
+        orderCostDisplay.textContent = `Стоимость заказа: ${totalCost}₽`;
+        orderCostDisplay.style.display = 'block'; // Показать стоимость, если есть сумма
+    } else {
+        orderCostDisplay.style.display = 'none'; // Скрыть стоимость, если сумма 0
+    }
+};
+
+const btn_order_update = () => {
     document.querySelectorAll('.dish_button').forEach(button => {
         button.addEventListener('click', (event) => {
             const dishCard = event.target.closest('.dish_container');
             const dishKeyword = dishCard.dataset.dish;
+
             const selectedDish = dishes.find(dish => dish.keyword === dishKeyword);
+            console.log(dishKeyword);
 
             if (selectedDish) {
                 selectedDishes[selectedDish.category] = selectedDish;
@@ -78,7 +93,4 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     });
-
-    // Инициализация при загрузке страницы
-    updateOrderDisplay();
-});
+}
